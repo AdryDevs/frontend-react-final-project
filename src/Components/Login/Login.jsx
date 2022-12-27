63130072d7f0848d0f0cdbcfb68738aa41cd775f
AdryDevs <alonsoadrian86@gmail.com>
2022-12-27 12:00:20 +0100

    feat: login component and css

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -19,36 +19,36 @@ function LoginComponent() {
       <MDBRow className='d-flex justify-content-center align-items-center h-100'>
         <MDBCol col='12'>
 
-          <MDBCard className='bg-dark text-white my-5 mx-auto' style={{borderRadius: '1rem', maxWidth: '400px'}}>
+          <MDBCard className='text-black my-5 mx-auto' style={{borderRadius: '1rem', maxWidth: '400px'}}>
             <MDBCardBody className='p-5 d-flex flex-column align-items-center mx-auto w-100'>
 
               <h2 className="fw-bold mb-2 text-uppercase">Welcome!</h2>
-              <p className="text-white-50 mb-5">Please enter your email and password</p>
+              <p className="text-black-50 mb-5">Please enter your email and password</p>
 
-              <MDBInput wrapperClass='mb-4 mx-5 w-100' labelClass='text-white' label='Email address' id='formControlLg' type='email' size="lg"/>
-              <MDBInput wrapperClass='mb-4 mx-5 w-100' labelClass='text-white' label='Password' id='formControlLg' type='password' size="lg"/>
+              <MDBInput wrapperClass='mb-4 mx-5 w-100' labelClass='text-black' label='Email address' id='formControlLg' type='email' size="lg"/>
+              <MDBInput wrapperClass='mb-4 mx-5 w-100' labelClass='text-black' label='Password' id='formControlLg' type='password' size="lg"/>
 
-              <p className="small mb-3 pb-lg-2"><a class="text-white-50" href="#!">Forgot password?</a></p>
-              <MDBBtn outline className='mx-2 px-5' color='white' size='lg'>
+              <p className="small mb-3 pb-lg-2"><a class="text-black-50" href="#!">Forgot password?</a></p>
+              <MDBBtn outline className='mx-2 px-5' color='black' size='lg'>
                 Login
               </MDBBtn>
 
               <div className='d-flex flex-row mt-3 mb-5'>
-                <MDBBtn tag='a' color='none' className='m-3' style={{ color: 'white' }}>
+                <MDBBtn tag='a' color='none' className='m-3' style={{ color: 'black' }}>
                   <MDBIcon fab icon='facebook-f' size="lg"/>
                 </MDBBtn>
 
-                <MDBBtn tag='a' color='none' className='m-3' style={{ color: 'white' }}>
+                <MDBBtn tag='a' color='none' className='m-3' style={{ color: 'black' }}>
                   <MDBIcon fab icon='twitter' size="lg"/>
                 </MDBBtn>
 
-                <MDBBtn tag='a' color='none' className='m-3' style={{ color: 'white' }}>
+                <MDBBtn tag='a' color='none' className='m-3' style={{ color: 'black' }}>
                   <MDBIcon fab icon='google' size="lg"/>
                 </MDBBtn>
               </div>
 
               <div>
-                <p className="mb-0">Don't have an account? <a href="#!" class="text-white-50 fw-bold">Register</a></p>
+                <p className="mb-0">Don't have an account? <a href="#!" class="text-black-50 fw-bold">Register</a></p>
 
               </div>
             </MDBCardBody>
